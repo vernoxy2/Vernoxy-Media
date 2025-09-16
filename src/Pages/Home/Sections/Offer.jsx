dd1637de63eeb09e9ee5e40098d678a8730d6835
vernoxy <vernoxy3@gmail.com>
2025-09-16 09:49:55 +0530

    1st Day

--- src/Pages/Home/Sections/Offer.jsx
+++ src/Pages/Home/Sections/Offer.jsx
@@ -10,7 +10,7 @@ const Offer = () => {
         className={"text-center flex justify-center "}
       ></Heading>
       <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
-        <div className="bg-gradient-to-b from-primary/30 to-transparent text-white h-[275px] rounded-lg border border-primary col-span-2"></div>
+        <div className="bg-gradient-to-b from-primary/30 to-transparent text-white h-[275px] rounded-lg border border-primary md:col-span-2"></div>
         <div className="bg-gradient-to-b from-primary/30 to-transparent text-white h-[275px] rounded-lg border border-primary "></div>
         <div className="bg-gradient-to-b from-primary/30 to-transparent text-white h-[275px] rounded-lg border border-primary"></div>
         <div className="bg-gradient-to-b from-primary/30 to-transparent text-white h-[275px] rounded-lg border border-primary"></div>
--- src/Pages/Home/Sections/Who.jsx
+++ src/Pages/Home/Sections/Who.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import img from '../../../assets/HomePageImgs/WhoImg.webp'
+import React from "react";
+import img from "../../../assets/HomePageImgs/WhoImg.webp";
+import Heading from "../../../Components/Heading";
+import PrimaryBtn from "../../../Components/PrimartyBtn";
 
 const Who = () => {
   return (
     <section>
-      <div className='container mx-auto'><img src={img} alt="" className='w-full' /></div>
-    </section>
-  )
-}
+      <div className="container mx-auto relative">
+        {/* Image */}
+        <img src={img} alt="Who We Are" className="w-full" />
 
-export default Who
+        {/* Overlay */}
+        <div className="absolute  top-0 left-0 w-full h-full bg-black/0"></div>
+
+        {/* Content over the overlay */}
+        <div className="absolute  top-0 left-0 w-full h-full flex flex-col gap-6 items-start justify-top text-white md:px-24 px-4 ml-auto">
+          <Heading
+            boldText={"Who"}
+            normalText={"We Are"}
+            className={"text-left"}
+          ></Heading>
+          <p className="max-w-md text-start">
+            At Vernoxy Media, we empower brands with data-driven creativity,
+            transforming ideas into impactful strategies. Through design,
+            analytics, and modern digital tools, we craft customized branding
+            and marketing solutions that drive growth, engagement, and lasting
+            success
+          </p>
+          <PrimaryBtn>Learn More</PrimaryBtn>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Who;
