f230315017a06b854a5b644d8376390c0006aaee
vernoxy <vernoxy3@gmail.com>
2025-09-30 14:34:12 +0530

    add Contact Page 3009

--- src/Components/ServiceSlider.jsx
+++ src/Components/ServiceSlider.jsx
@@ -30,9 +30,7 @@ const ServiceSlider = () => {
           ))}
         </div>
       </div>
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-vernoxy via-transparent to-vernoxy">
-
-      </div>
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-vernoxy via-transparent to-vernoxy"></div>
     </div>
   );
 };
--- src/Pages/Services/DigitalMarketing/DigitalMarketing.jsx
+++ src/Pages/Services/DigitalMarketing/DigitalMarketing.jsx
@@ -6,6 +6,7 @@ import bannerImg from "../../../assets/ServicePageImg/BannerImg/DM.webp"
 import FatureKey from "../../../Components/FatureKey";
 import keyBanner from "../../../assets/ServicePageImg/FatureKey/Key1.webp"
 import ServiceSlider from "../../../Components/ServiceSlider";
+import PrimaryProcess from "../../../Components/PrimaryProcess";
 
 const KeyItems = [
   "Content Marketing That Tells Your Brand's Story",
@@ -26,6 +27,7 @@ const DigitalMarketing = () => {
       <ServiceSlider />
       <PrimaryBanner boldText={"Driving Growth Through"} normalText={"Data-Driven Marketing Solutions"} bannerImg={bannerImg} description={"Our digital marketing services are designed to elevate your brand and reach your target audience effectively. We develop customized strategies that include SEO, PPC advertising, social media marketing, content marketing, and email campaigns. By leveraging data-driven insights and the latest trends, we maximize engagement, drive traffic, and boost conversions, ensuring a measurable return on your investment. Let us help you grow your digital presence and achieve your business goals."} />
       <FatureKey boldText={"Key Feature of "} normalText={"Digital Marketing"} Keybanner={keyBanner} KeyList={KeyItems}/>
+      <PrimaryProcess/>
       <AIRevolution />
     </div>
   );
