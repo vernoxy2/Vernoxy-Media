4cf3ac5206c6e8fe1bc5f8fd2c5f3b301850834d
vernoxy <vernoxy3@gmail.com>
2026-01-07 17:17:06 +0530

    Fix: city Routes

---
+++ src/Data/CityData.js
@@ -0,0 +1,33 @@
+import SuratImg from "../assets/CityDataImg/Surat.jpg";
+import AhmedabadImg from "../assets/CityDataImg/Ahmedabad.jpg";
+import Vadodara from "../assets/CityDataImg/Vadodara.jpg";
+import Gandhinagar from "../assets/CityDataImg/Gandhinagar.jpg";
+import Vapi from "../assets/CityDataImg/Vapi.webp";
+const cities = [
+  {
+    name: "surat",
+    image: SuratImg,
+  },
+  {
+    name: "vapi",
+    image: Vapi,
+  },
+  {
+    name: "Ahmedabad",
+    image: AhmedabadImg,
+  },
+  {
+    name: "Vadodara",
+    image: Vadodara,
+  },
+  {
+    name: "Gandhinagar",
+    image: Gandhinagar,
+  },
+  {
+    name: "Anand",
+    image: SuratImg,
+  },
+];
+
+export default cities;
--- src/Pages/GlobalPresence/GlobalPresence.jsx
+++ src/Pages/GlobalPresence/GlobalPresence.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ProductsCard from "./Sections/ProductsCard";
 import StateComponent from '../../Components/StateComponent';
+import CityDisplay from '../../Components/CityDisplay';
 
 const GlobalPresence = () => {
   return (
     <div>
       <ProductsCard />
       {/* <StateComponent/> */}
+      {/* <CityDisplay/> */}
     </div>
   )
 }
